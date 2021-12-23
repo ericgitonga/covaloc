7d0d40d4eacc88162a7d09a5bdb3432bfe4da37e
Eric Gitonga <gitonga@gmail.com>
2021-12-23 14:33:18 +0300

    add data

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("data/hospitals.csv")
+
+if st.checkbox("Click to show data"):
+    st.dataframe(df)
 
 st.title("Covid Vaccine Locator")
 
@@ -6,4 +12,4 @@ with st.form("Vaccine Locations"):
     hospital = st.text_input("Fill in name of hospital")
     st.form_submit_button("Submit")
 
-st.write(hospital)
+st.table(df[df["Name"] == hospital])

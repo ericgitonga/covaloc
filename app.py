cb3937c960abe824f6ccfbe32a0a950d54e5ee29
Eric Gitonga <gitonga@gmail.com>
2021-12-23 13:07:55 +0300

    start adding elements to form

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Covid Vaccine Locator")
 
 with st.form("Vaccine Locations"):
-    selection = st.selectbox("Choose", ["A", "B"])
+    hospital = st.text_input("Fill in name of hospital")
     st.form_submit_button("Submit")
 
-st.write(selection)
+st.write(hospital)
